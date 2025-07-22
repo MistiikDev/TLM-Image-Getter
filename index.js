96d6fbf30bb3299747e10335842ed5119caa4617
DAF <91028158+MistiikDev@users.noreply.github.com>
2025-07-22 12:46:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const prompt = require('prompt');
 
 prefix = "https://tiermaker.com/"
 
-// Use Puppeteer plugins for stealth and user-agent anonymization
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const ua_anonymizer = require('puppeteer-extra-plugin-anonymize-ua');
 
@@ -16,11 +15,6 @@ puppeteer.use(StealthPlugin()).use(ua_anonymizer());
 prompt.start();
 
 async function main(set_url = "https://tiermaker.com/create/fnaf-characters") {
-    // Optionally ask the user for a URL
-    // prompt.get(['URL'], async function(err, result) {
-    //     if (err) { return onErr(err) }
-    //     url = result.URL;
-
     const url = set_url;
 
     const browser = await puppeteer.launch({
@@ -30,15 +24,12 @@ async function main(set_url = "https://tiermaker.com/create/fnaf-characters") {
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: 'domcontentloaded' });
 
-    // Wait for the image carousel or container selector to load
     await page.waitForSelector('#create-image-carousel', { visible: true });
 
-    // Get the raw HTML from the page
     const data = await page.evaluate(() => {
         return document.querySelector('#create-image-carousel').innerHTML;
     });
 
-    // Load the HTML data into Cheerio for further parsing
     let $ = cheerio.load(data);
     const pngUrls = [];
 
@@ -55,7 +46,6 @@ async function main(set_url = "https://tiermaker.com/create/fnaf-characters") {
 
     console.log(pngUrls)
 
-    // If URLs were found, download them
     if (pngUrls.length > 0) {
         console.log("Found PNG URLs:", pngUrls);
         await downloadImages(pngUrls);
@@ -66,13 +56,11 @@ async function main(set_url = "https://tiermaker.com/create/fnaf-characters") {
     await browser.close();
 }
 
-// Function to download images from URLs
 async function downloadImages(urls) {
     for (const url of urls) {
         const fileName = path.basename(url + ".png");
         const filePath = path.join(__dirname, 'downloaded_images', fileName);
 
-        // Create the 'downloaded_images' directory if it doesn't exist
         if (!fs.existsSync(path.dirname(filePath))) {
             fs.mkdirSync(path.dirname(filePath), { recursive: true });
         }
